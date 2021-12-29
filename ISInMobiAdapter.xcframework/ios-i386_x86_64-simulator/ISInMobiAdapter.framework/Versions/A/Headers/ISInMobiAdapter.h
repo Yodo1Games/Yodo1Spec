9a74249f292cea50531040f72dd9a5f758f8f6d4
黄艺贤 <hyx691@163.com>
2021-12-29 15:08:18 +0800

    v6.0.10

---
+++ ISInMobiAdapter.xcframework/ios-i386_x86_64-simulator/ISInMobiAdapter.framework/Versions/A/Headers/ISInMobiAdapter.h
@@ -0,0 +1,22 @@
+//
+//  ISInMobiAdapter.h
+//  ISInMobiAdapter
+//
+//  Created by Yotam Ohayon on 26/10/2015.
+//  Copyright © 2015 IronSource. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IronSource/ISBaseAdapter+Internal.h"
+
+static NSString * const InMobiAdapterVersion = @"4.3.13";
+static NSString * GitHash = @"d297e31d5";
+ 
+//System Frameworks For InMobi Adapter
+
+@import WebKit;
+
+@interface ISInMobiAdapter : ISBaseAdapter
+
+
+@end
