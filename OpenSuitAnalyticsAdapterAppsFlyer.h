b299e17b2ae6ac6bfa5b5c461f061bafef872a58
黄艺贤 <hyx691@163.com>
2022-01-21 20:13:58 +0800

    v1.0.0

---
+++ OpenSuitAnalyticsAdapterAppsFlyer.h
@@ -0,0 +1,14 @@
+//
+//  OpenSuitAnalyticsAdapterAppsFlyer.h
+//
+//  Created by hyx on 14-10-14.
+//  Copyright (c) 2014年 yodo1. All rights reserved.
+//
+
+#import "OpenSuitAnalyticsAdapter.h"
+
+@interface OpenSuitAnalyticsAdapterAppsFlyer : OpenSuitAnalyticsAdapter
+
++ (OpenSuitAnalyticsType)OpenSuitAnalyticsType;
+
+@end
