8014ed428a45553839182830932a6e8821d939cd
黄艺贤 <hyx691@163.com>
2022-01-22 09:43:59 +0800

    no message

---
+++ OpenSuitAnalyticsAdapterTalkingData.h
@@ -0,0 +1,14 @@
+//
+//  OpenSuitAnalyticsAdapterTalkingData.h
+//
+//  Created by hyx on 14-10-14.
+//  Copyright (c) 2014年 yodo1. All rights reserved.
+//
+
+#import "OpenSuitAnalyticsAdapter.h"
+
+@interface OpenSuitAnalyticsAdapterTalkingData : OpenSuitAnalyticsAdapter
+
++ (OpenSuitAnalyticsType)OpenSuitAnalyticsType;
+
+@end
